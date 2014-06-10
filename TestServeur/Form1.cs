01a40cb23a61cfa1eb6fdd3feaf90c45dd0f7bd5
Eptwalabha <damiengabrielle@gmail.com>
2014-06-10 15:30:10 +0200

    Ajout du serveur de test

---
+++ TestServeur/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using CommonConnection;
+
+namespace TestServeur
+{
+    public partial class Form1 : Form, IServerListener
+    {
+        private ModuleServeur moduleServeur;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moduleServeur = new ModuleServeur(this);
+            moduleServeur.StartServer(IPAddress.Parse("192.168.43.66"), 8888);
+
+        }
+
+        public byte[] ProcessDataFromClient(byte[] dataFromClient, int dataSize)
+        {
+            // throw new NotImplementedException();
+            Console.WriteLine(Encoding.ASCII.GetString(dataFromClient));
+            return dataFromClient;
+        }
+
+        public void MessageForListener(string dataFromClient)
+        {
+            Console.WriteLine(dataFromClient);
+        }
+
+        private byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        private string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            moduleServeur.StopModule();
+        }
+    }
+}
